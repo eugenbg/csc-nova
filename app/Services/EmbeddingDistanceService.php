80ef248bb97f1061109478e302dda6476654f357
eugenbg <eugen.bogdanovich@gmail.com>
2022-05-07 14:16:21 +0100

    embeddings test

--- app/Services/EmbeddingDistanceService.php
+++ app/Services/EmbeddingDistanceService.php
@@ -15,4 +15,29 @@ class EmbeddingDistanceService {
         return sqrt($sum);
 
     }
+
+    public static function getCosineSimilarity($left, $right): float
+    {
+        $xy = 0;
+        foreach ($left as $key => $number1) {
+            $number2 = $right[$key];
+            $xy += $number1 * $number2;
+        }
+
+        $xSum = 0;
+        foreach ($left as $number1) {
+            $xSum += $number1 ** 2;
+        }
+
+        $xSum = sqrt($xSum);
+
+        $ySum = 0;
+        foreach ($right as $number1) {
+            $ySum += $number1 ** 2;
+        }
+
+        $ySum = sqrt($xSum);
+
+        return $xy / ($xSum * $ySum);
+    }
 }
